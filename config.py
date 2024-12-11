9eed67ddaba2433e43d48e685c397ea4ec10dc86
Carlo Bosio <carlobosioit@gmail.com>
2024-12-10 16:07:02 -0800

    adding env parameters

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 config = {
     "agents": {
         "attraction_weight": 1.05,
@@ -5,20 +7,26 @@ config = {
         "repulsion_weight_shepherd": 1,
         "heading_inertia": 0.5,
         "noise_level": 0.05,
-        "neighbor_count": 5,
-        "shepherd_detection_dist": 65,
+        "neighbor_count": 8,
+        "shepherd_detection_dist": 40,
         "agent_interaction_dist": 2,
         "angular_noise": 0.3,
-        "disp_per_timestep": 1,
+        "disp_per_timestep": 0.3,
         "grazing_probability": 0.05
     },
     "shepherd": {
         "disp_per_timestep": 1.5,
         "angular_noise": 0.3,
-        "group_distance_factor": 1.0
+        "group_distance_factor": 1.0,
+        "heading_inertia": 0.5,
+        "target": np.zeros((2,))
     },
     "environment": {
         "n_agents": 10,
-        "n_shepherds": 1
+        "n_shepherds": 1,
+        "x_min": -100,
+        "x_max": 100,
+        "y_min": -100,
+        "y_max": 100
     }
 }
